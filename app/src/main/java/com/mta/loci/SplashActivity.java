6cfdc1979c924cbaba613223e1eb86e035ef6006
Yarden Ben Shimon <yardenbs2@gmail.com>
2018-09-01 01:28:04 +0300

    added FirebaseUI login in SplashActivity changing splash logic from timer to launcher theme

--- app/src/main/java/com/mta/loci/SplashActivity.java
+++ app/src/main/java/com/mta/loci/SplashActivity.java
@@ -3,23 +3,65 @@ package com.mta.loci;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SplashActivity extends Activity {
 
     private static int SPLASH_TIME_OUT = 4000;
+    private static int RC_SIGN_IN = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_Launcher);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent homeIntent = new Intent(SplashActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
-            }
-        }, SPLASH_TIME_OUT);
+        login();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK){
+                //TODO: Go to homeActivity
+            }
+            else if(resultCode == RESULT_CANCELED){
+                Toast.makeText(this,"Sign in canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void login() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if(user != null){
+            //TODO: Go to homeActivity
+            Toast.makeText(this, user.getDisplayName(), Toast.LENGTH_SHORT).show();
+        }
+        else {
+            firebaseUILogin();
+        }
+    }
+
+    private void firebaseUILogin() {
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
 }
