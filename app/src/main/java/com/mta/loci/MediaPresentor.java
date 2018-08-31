09ee3b0df90f9f0ee4bc8c343d5b8bd029636b10
AmiStrn <123amitai@gmail.com>
2018-08-31 03:44:17 +0300

    added Media Interface

---
+++ app/src/main/java/com/mta/loci/MediaPresentor.java
@@ -0,0 +1,7 @@
+package com.mta.loci;
+
+interface MediaPresentor {
+
+    abstract void show(); //show an image/video/text-file/sound
+                          // (1 method? maybe should do a lambda expression instead?)
+}
--- app/src/main/java/com/mta/loci/Post.java
+++ app/src/main/java/com/mta/loci/Post.java
@@ -2,8 +2,10 @@ package com.mta.loci;
 
 class Post {
     //members: UserId, Location, Media (Interface)
+    String UserId;
 
-    Post() {
+    Post(String UserId) {
         //add inputs for c'tor
+        this.UserId = UserId;
     }
 }
