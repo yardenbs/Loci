f27c371b5b3c0a0bbb36135d6f357af910418481
AmiStrn <123amitai@gmail.com>
2018-10-14 00:38:10 +0300

    new activity - PostPublishActivity - after creating the Post object this activity is where the user decides to post to Loci or to discard the post.

---
+++ app/src/main/java/com/mta/loci/PostPublishActivity.java
@@ -0,0 +1,13 @@
+package com.mta.loci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PostPublishActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_publish);
+    }
+}
