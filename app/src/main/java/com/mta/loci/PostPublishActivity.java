fea475db3529f00ccec5cc234f775758573c696d
AmiStrn <123amitai@gmail.com>
2018-10-14 01:42:32 +0300

    last commit for the day - started work on the publishing activity

--- app/src/main/java/com/mta/loci/PostPublishActivity.java
+++ app/src/main/java/com/mta/loci/PostPublishActivity.java
@@ -3,11 +3,31 @@ package com.mta.loci;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class PostPublishActivity extends AppCompatActivity {
+
+    private LatLng mPostLocation;
+    private LociUser mUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        LociUtil.InitUserFromIntent(getIntent(), mUser);
+        mPostLocation = getIntent().getExtras().getParcelable("4");
         setContentView(R.layout.activity_post_publish);
+    }
+
+    private void uploadPostToDatabase(String url, String mediaType ){
+        DatabaseReference databasePosts = FirebaseDatabase.getInstance().getReference("posts");
+
+        String id = databasePosts.push().getKey();
+        //TODO: replace the following place holder !!!
+        Post post = new Post(id, "replace this string with: mUser.GetUserId()", mPostLocation.latitude, mPostLocation.longitude, url, mediaType);
+
+        databasePosts.child(id).setValue(post);
+
     }
 }
