48c8bfd5ed6bf5592f4716a21fe575c6b2e00cca
Yarden Ben Shimon <yardenbs2@gmail.com>
2018-10-19 06:02:39 +0300

    LociPostActivity

--- app/src/main/java/com/mta/loci/LociPostActivity.java
+++ app/src/main/java/com/mta/loci/LociPostActivity.java
@@ -1,30 +1,57 @@
 package com.mta.loci;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import static com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade;
+
 public class LociPostActivity extends AppCompatActivity {
+
+    private  Post mPost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loci_post);
-        InitPostDataFromGridClick();
+        initUI();
     }
 
-    private void InitPostDataFromGridClick() {
+    private void initUI() {
         setContentView(R.layout.activity_loci_post);
 
-        String title = getIntent().getStringExtra("title");
-        Bitmap bitmap = getIntent().getParcelableExtra("image");
+        String creatorId = getIntent().getStringExtra("creatorId");
+        String postId = getIntent().getStringExtra("postId");
 
-        TextView titleTextView = (TextView) findViewById(R.id.title);
-        titleTextView.setText(title);
 
-        ImageView imageView = (ImageView) findViewById(R.id.image);
-        imageView.setImageBitmap(bitmap);
+        ImageView postImageView = (ImageView) findViewById(R.id.image);
+       mPost =  PostUtils.getPostFromDatabase(postId, creatorId);
+       if(mPost != null) {
+           loadImage(mPost.getMediaUrl(), postImageView);
+           TextView creatorTextView = (TextView) findViewById(R.id.creator);
+           LociUser creator =  LociUtil.getUserFromDatabase(creatorId);
+           creatorTextView.setText(creator.getName());
+       }
+
+    }
+
+    private   void loadImage(String url, ImageView imageView) {
+        Context context = imageView.getContext();
+        ColorDrawable cd = new ColorDrawable(ContextCompat.getColor(context, R.color.common_google_signin_btn_text_light_default));
+        Glide.with(context)
+                .load(url)
+                .apply(new RequestOptions()
+                        .placeholder(cd)
+                        .centerCrop())
+                .transition(withCrossFade())
+                .into(imageView);
     }
 }
